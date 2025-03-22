e0999a354543f7c2c653925652948441d0187635
Joel Lehman <joel@MacBook-Pro-31.local>
2025-03-22 10:52:44 -0700

    changed dir structure

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ base_dir = '/Users/joel/Downloads/reviews'
 data_dir = '/Users/joel/code/examples' 
 model_dir = '/Users/joel/code/bookformer'
 embedding_dir = '/Users/joel/code/examples/embeddings'
+classifier_dir = '/Users/joel/code/examples/classifiers'
 
 try:
     from _secrets import openai_api_key
